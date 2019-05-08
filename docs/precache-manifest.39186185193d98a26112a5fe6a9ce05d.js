9e6055be47452af223e1e4c4d683dd307098876b
ruinername <russhianhere@gmail.com>
2019-05-08 20:58:58 +0300

    service

---
+++ docs/precache-manifest.39186185193d98a26112a5fe6a9ce05d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "be3b55c63dd60c4207315326f47ddc2b",
+    "url": "/cinema-service/static/media/banner-1.be3b55c6.png"
+  },
+  {
+    "revision": "d3e1dffec9d71deb13f6",
+    "url": "/cinema-service/static/js/runtime~main.d3e1dffe.js"
+  },
+  {
+    "revision": "9c2c6fd85c199ea08470",
+    "url": "/cinema-service/static/js/main.9c2c6fd8.chunk.js"
+  },
+  {
+    "revision": "fd85d6848e9889ae1b97",
+    "url": "/cinema-service/static/js/2.fd85d684.chunk.js"
+  },
+  {
+    "revision": "fd85d6848e9889ae1b97",
+    "url": "/cinema-service/static/css/2.a77911eb.chunk.css"
+  },
+  {
+    "revision": "4454689e53839039416d072e633c6b36",
+    "url": "/cinema-service/index.html"
+  }
+];
