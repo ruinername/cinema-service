c7f3e4a940df80dc140b56f8e8947ac091c76bd0
balanda <baalaandaa@gmail.com>
2019-05-30 19:50:57 +0300

    my first time.....

---
+++ build/precache-manifest.90e567121feb7f266e4ba8f7fb1740f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "be3b55c63dd60c4207315326f47ddc2b",
+    "url": "/cinema-service/static/media/banner-1.be3b55c6.png"
+  },
+  {
+    "revision": "d3e1dffec9d71deb13f6",
+    "url": "/cinema-service/static/js/runtime~main.ae298d01.js"
+  },
+  {
+    "revision": "a7c01ecd93237fe5bf21",
+    "url": "/cinema-service/static/js/main.56e5d552.chunk.js"
+  },
+  {
+    "revision": "616520b7af482ed68dc3",
+    "url": "/cinema-service/static/js/2.419d79c1.chunk.js"
+  },
+  {
+    "revision": "616520b7af482ed68dc3",
+    "url": "/cinema-service/static/css/2.44721667.chunk.css"
+  },
+  {
+    "revision": "899a8e7b0be5e4aa28aaff754743d824",
+    "url": "/cinema-service/index.html"
+  }
+];
